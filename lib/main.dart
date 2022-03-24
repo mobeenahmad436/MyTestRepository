a2d9c0dcda3dd614ef240a79ffe0c7dbbbb51794
Mubeen Ahmad <mubeen.ahmad@globalsni.com>
2022-03-24 15:17:23 +0500

    day 2

--- lib/main.dart
+++ lib/main.dart
@@ -11,28 +11,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:  MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title:const Text("Awesome Widget")
+      appBar: AppBar(title: const Text("Catalog App")),
+      drawer:  const Drawer(
+        // backgroundColor: Colors.blue,
       ),
       body: Container(
-        color: Colors.purple,
-
       ),
+
     );
   }
 }
